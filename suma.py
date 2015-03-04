f92001afca3e022b3f593c65739aa9a85f2beccc
jesmoreno <jesusmorenopinto@gmail.com>
2015-03-04 17:51:25 +0100

    Create suma.py

---
+++ suma.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class suma:
+
+    def parse(self, request, rest):
+        
+        try:
+            numeros = rest.split('/')
+            numero1 = int(numeros[0])
+            numero2 = int(numeros[1])
+
+        except ValueError:
+            return None
+        return (numero1,numero2)
+
+    def process(self, parsedRequest):
+        if not parsedRequest:
+
+            return("400 Bad Request" ,
+                   "<html><body><h1>Error!</h1>" +
+                   "</body></html>")
+
+        resultado = (str(parsedRequest[0]) + "+" + str(parsedRequest[1]) +
+                    "=" + str(parsedRequest[0]+parsedRequest[1]))
+
+        return("200 OK",
+               "<html><body>" +
+               "<h1>" + resultado +"</h1>" +
+               "</body></html>")         
+
